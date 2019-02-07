28f8f18076e56c3deaae62e32961bf35d2edf459
Paul Gearo <PaulGearo@gmail.com>
2019-02-07 10:13:06 -0600

    Add project files.

---
+++ Calculator Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculator_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
